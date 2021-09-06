0ce00422fdf2c523aea940311756bf419e656e32
nass92 <goujanassim@hotmail.com>
2021-09-06 13:24:51 +0200

    ewe

--- src/components/Log & footer/Footer.js
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-  return (
-    <footer className="footer-background text-center text-white p-3 mt-auto fixed-bottom">
-      Made by Nassim For Nelson Makamo
-    </footer>
-  );
-};
-
-export default Footer;
